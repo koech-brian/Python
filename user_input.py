c5f3625cc98ccfd0c915b97207a4c4d002b4f923
Brian Koech <156711435+koech-brian@users.noreply.github.com>
2024-02-27 17:46:02 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Use input() to get user's name
+name = input("Enter your name:Brian ")
+
+# Use input() to get user's age
+age = input("Enter your age: 25")
+
+# Use input() to get user's location
+location = input("Enter your location:Nairobi ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
